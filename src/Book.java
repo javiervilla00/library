494f448816129b1cb0350893e0cd0baa7c62e984
javiervilla00 <javiervilla2000.jv@gmail.com>
2022-11-03 18:54:12 +0100

    Comienzo el proyecto

---
+++ src/Book.java
@@ -0,0 +1,47 @@
+public class Book {
+    private Integer isbn;
+    private String title;
+    private String author;
+    private String genre;
+    private String publishDate;
+
+    public Integer getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(Integer isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
+}
