494ed4c31648a4bd98ea4ef1d004a0701b0053cb
javiervilla00 <javiervilla2000.jv@gmail.com>
2022-11-07 18:25:49 +0100

    Comienzo el proyecto

---
+++ Author.java
@@ -0,0 +1,38 @@
+public class Author {
+    private Integer name;
+    private Integer surname;
+    private String birthDate;
+    private String placeBirth;
+
+    public Integer getName() {
+        return name;
+    }
+
+    public void setName(Integer name) {
+        this.name = name;
+    }
+
+    public Integer getSurname() {
+        return surname;
+    }
+
+    public void setSurname(Integer surname) {
+        this.surname = surname;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPlaceBirth() {
+        return placeBirth;
+    }
+
+    public void setPlaceBirth(String placeBirth) {
+        this.placeBirth = placeBirth;
+    }
+}
