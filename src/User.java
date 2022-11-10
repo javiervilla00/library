da60ee35e93ec01eb853fdded9d9cb95debb3cb7
javiervilla00 <javiervilla2000.jv@gmail.com>
2022-11-10 17:23:48 +0100

    Comienzo el proyecto.

---
+++ src/User.java
@@ -0,0 +1,56 @@
+public class User {
+    private Integer dni;
+    private String name;
+    private String surname;
+    private String phone;
+    private String adress;
+    private String poblation;
+
+    public Integer getDni() {
+        return dni;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getPoblation() {
+        return poblation;
+    }
+
+    public void setPoblation(String poblation) {
+        this.poblation = poblation;
+    }
+}
